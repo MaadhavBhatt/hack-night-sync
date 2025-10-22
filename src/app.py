9c6081aedfabea0f6358bfe36b60b5e939cfc78d
Maadhav Bhatt <81471860+MaadhavBhatt@users.noreply.github.com>
2025-10-22 21:06:33 +0530

    feat: add config module with environment variable checks, requirements.txt, and featureless app.py

---
+++ src/app.py
@@ -0,0 +1,12 @@
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+from .config import check_environment_variables
+
+
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+if __name__ == "__main__":
+    check_environment_variables()
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
---
+++ src/config.py
@@ -0,0 +1,40 @@
+import os
+
+ENV_VARS_CHECKED = False
+CONFIG: dict = {}
+
+
+def check_environment_variables() -> None:
+    """
+    Checks if the required environment variables are set for the application.
+
+    Raises:
+        ValueError: If any of the required environment variables are not set or if the Firebase credentials file does not exist.
+
+    This function checks for the following environment variables:
+        - SLACK_BOT_TOKEN: The token for the Slack bot.
+        - SLACK_APP_TOKEN: The token for the Slack app.
+    """
+    required_vars = [
+        "SLACK_BOT_TOKEN",
+        "SLACK_APP_TOKEN",
+    ]
+
+    optional_vars = []
+
+    for var in required_vars:
+        if os.environ.get(var) is None:
+            raise ValueError(f"{var} environment variable is not set.")
+
+    if optional_vars:
+        global CONFIG
+        for var in optional_vars:
+            if os.environ.get(var) is None:
+                os.environ[var] = "false"
+                print(
+                    f"Optional environment variable {var} is not set. Defaulting to false."
+                )
+            CONFIG[var.lower()] = os.environ.get(var).lower() == "true"
+
+    global ENV_VARS_CHECKED
+    ENV_VARS_CHECKED = True
